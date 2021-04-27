f64d5436e410ff9d85863832a2bc216fb9017c0f
Mineirin <60952860+Mineirin@users.noreply.github.com>
2021-04-26 21:39:32 -0300

    Add files via upload

---
+++ SorteioMagnata/SorteioMagnata/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace SorteioMagnata
+{
+    static class Program
+    {
+        //DECLARAÇÃO DE VARIÁVEIS GLOBAIS DO SISTEMA
+        public static string nomeUsuario;
+        public static string cargoUsuario;
+
+        /// <summary>
+        /// Ponto de entrada principal para o aplicativo.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FrmLogin());
+        }
+    }
+}
